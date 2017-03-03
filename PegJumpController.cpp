3ff44ed496ae9f47374eb1344063a70ee8e9918d
Zachary Wileman <zdw27f@mst.edu>
2017-03-03 15:30:44 -0600

    Changes made:
    -Started on the controller
    -Added code for user to select the game they want to play
    -Implemented functions from View into Controller

--- PegJumpController.cpp
+++ PegJumpController.cpp
@@ -1,2 +1,37 @@
 //Group Members: Alfonso Miguel Santos-Tankia and Zachary Wileman
 #include "PegJumpController.h"
+#include "PegJumpView.h"
+
+const int numGames = 1;
+
+int main()
+{
+  PegJumpView View;
+  int gameScore[numGames] = {0};
+	int gameSelected;
+
+  View.printStart();
+
+	do
+	{
+		do
+		{
+      View.gameSelect();
+
+			if (!(cin >> gameSelected))
+			{
+				cin.clear();
+				cin.ignore();
+			}
+
+			if (gameSelected == 9)
+				View.printMenu(gameScore);
+
+			else if (gameSelected != 1)
+        View.invalidChoice();
+
+		} while (gameSelected != 1);
+  }
+
+  return 0;
+}
